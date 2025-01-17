3856bb9ec33a9b75ed310e6b4308f013d76a21e9
fles <anotherfles@gmail.com>
2025-01-17 13:57:50 +0300

    Add models

---
+++ src/Services/Offices.API/Offices.Core/Models/Receptionist.cs
@@ -0,0 +1,13 @@
+﻿namespace Offices.Core.Models;
+
+public class Receptionist
+{
+	public Guid Id { get; set; }
+	public string FirstName { get; set; }
+	public string LastName { get; set; }
+	public string MiddleName { get; set; }
+
+	public Guid AccountId { get; set; }
+	public Guid OfficeId { get; set; }
+	public Office Office { get; set; }
+}
