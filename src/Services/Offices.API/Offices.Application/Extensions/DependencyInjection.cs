172f7888a7c3ac5a20965809de49b376bbec08f1
fles <anotherfles@gmail.com>
2025-01-17 15:45:37 +0300

    Add application layer di extension

---
+++ src/Services/Offices.API/Offices.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Offices.Application.MapperProfiles;
+using System.Reflection;
+
+namespace Offices.Application.Extensions;
+
+public static class DependencyInjection
+{
+	public static void ConfigureAutomapper(this IServiceCollection services)
+	{
+		services.AddAutoMapper(config =>
+		{
+			config.AddProfile<OfficeMapperProfile>();
+		});
+	}
+
+	public static void ConfigureMediatr(this IServiceCollection services)
+	{
+		services.AddMediatR(config =>
+		{
+			config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+		});
+	}
+}
