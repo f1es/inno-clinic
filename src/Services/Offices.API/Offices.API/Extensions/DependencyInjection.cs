2318f2af29c46c5f67df654b75fc869ef56ba6b0
fles <anotherfles@gmail.com>
2025-01-17 16:50:08 +0300

    Add di extensions for api layer

---
+++ src/Services/Offices.API/Offices.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Offices.Infrastructure.Context;
+
+namespace Offices.API.Extensions;
+
+public static class DependencyInjection
+{
+	public static void ConfigureDbContext(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		services.AddDbContext<OfficesDbContext>(options =>
+		{
+			options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString"));
+		});
+	}
+}
