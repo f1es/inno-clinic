2e36dbb42833515a7dfdd6cd0f1b36aebfae5fe1
fles <anotherfles@gmail.com>
2025-01-17 15:38:20 +0300

    Add infrastructure di extension

---
+++ src/Services/Offices.API/Offices.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Offices.Core.Repositories;
+using Offices.Infrastructure.Repositories;
+
+namespace Offices.Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+	public static void ConfigureRepositories(this IServiceCollection services)
+	{
+		services.AddScoped<IUnitOfWork, UnitOfWork>();
+	}
+}
