c585ef7c5d20e71c7519ec939ce8ec92f419c962
fles <anotherfles@gmail.com>
2025-01-17 14:04:02 +0300

    Add db context and tables configuration

---
+++ src/Services/Offices.API/Offices.Infrastructure/Configuration/ReceptionistsConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Offices.Core.Models;
+
+namespace Offices.Infrastructure.Configuration;
+
+public class ReceptionistsConfiguration : IEntityTypeConfiguration<Receptionist>
+{
+	public void Configure(EntityTypeBuilder<Receptionist> builder)
+	{
+		builder.HasKey(x => x.Id);
+
+		builder.Property(x => x.FirstName)
+			.IsRequired();
+
+		builder.Property(x => x.LastName)
+			.IsRequired();
+
+		builder.Property(x => x.AccountId)
+			.IsRequired();
+
+
+		builder.HasOne(x => x.Office)
+			.WithOne(x => x.Receptionist)
+			.HasForeignKey<Receptionist>(x => x.OfficeId);
+	}
+}
---
+++ src/Services/Offices.API/Offices.Infrastructure/Context/OfficesDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Offices.Core.Models;
+using System.Reflection;
+
+namespace Offices.Infrastructure.Context;
+
+public class OfficesDbContext : DbContext
+{
+	public DbSet<Office> Offices { get; set; }
+	public DbSet<Receptionist> Receptionists { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+	}
+}
