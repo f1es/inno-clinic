ff08b0ea42f96d4aee833101ff7880da31c96979
fles <anotherfles@gmail.com>
2025-01-22 17:47:40 +0300

    Made initialize migration, database docker image, add db context to di, add connection string

---
+++ src/Services/Services.API/Services.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Services.Infrastructure.Context;
+
+namespace Services.API.Extensions;
+
+public static class DependencyInjection
+{
+	public static void ConfigureDbContext(this IServiceCollection services, WebApplicationBuilder builder) =>
+		services.AddDbContext<ServicesDbContext>(options =>
+		{
+			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+		});
+}
