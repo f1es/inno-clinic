687515c206531aa644e40dfaa3f7ca323839ece0
fles <anotherfles@gmail.com>
2025-01-24 14:46:55 +0300

    Add database di extension, docker compose for data base, made initizlize migration

---
+++ src/Services/Authorization.API/Authorization.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Authorization.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authorization.API.Extensions;
+
+public static class DependencyInjection
+{
+	public static void ConfigureDbContext(this IServiceCollection services, WebApplicationBuilder builder) =>
+		services.AddDbContext<AuthorizationDbContext>(option =>
+		{
+			option.UseSqlServer(builder.Configuration.GetConnectionString("ConnectionString"));
+		});
+}
