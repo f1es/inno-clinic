8e59a9325322e2ad27407842eeec0a3e5992c634
fles <anotherfles@gmail.com>
2025-01-24 17:12:07 +0300

    Add controller description

--- src/Services/Authorization.API/Authorization.API/Controllers/AccountController.cs
+++ src/Services/Authorization.API/Authorization.API/Controllers/AccountController.cs
@@ -15,23 +15,48 @@ public class AccountController : ControllerBase
 		_accountService = accountService;
 	}
 
+	/// <summary>
+	/// Registration account method
+	/// </summary>
+	/// <param name="registerAccountRequestDto">Registration account request data transfer object</param>
+	/// <returns></returns>
 	[HttpPost("register")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Register(RegisterAccountRequestDto registerAccountRequestDto)
 	{
 		await _accountService.RegisterAsync(registerAccountRequestDto);
 
-		return NoContent();
+		return Ok();
 	}
 
+	/// <summary>
+	/// Login account method
+	/// </summary>
+	/// <param name="loginAccountRequestDto">Login account request data transfer object</param>
+	/// <returns></returns>
 	[HttpPost("login")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Login(LoginAccountRequestDto loginAccountRequestDto)
 	{
 		var token = await _accountService.LoginAsync(loginAccountRequestDto);
 
+		Response.Cookies.Append("sec", token);
+
 		return Ok(token);
 	}
 
+	/// <summary>
+	/// Get accounts method
+	/// </summary>
+	/// <returns></returns>
 	[HttpGet]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> GetAll()
 	{
 		var accounts = await _accountService.GetAllAsync();
@@ -39,7 +64,15 @@ public class AccountController : ControllerBase
 		return Ok(accounts);
 	}
 
+	/// <summary>
+	/// Get account by id method
+	/// </summary>
+	/// <param name="id">Account's unique identifier</param>
+	/// <returns></returns>
 	[HttpGet("{id:guid}")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> GetById(Guid id)
 	{
 		var account = await _accountService.GetByIdAsync(id);
@@ -47,7 +80,15 @@ public class AccountController : ControllerBase
 		return Ok(account);
 	}
 
+	/// <summary>
+	/// Delete account method
+	/// </summary>
+	/// <param name="id">Account's unique identifier</param>
+	/// <returns></returns>
 	[HttpDelete("{id:guid}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Delete(Guid id)
 	{
 		await _accountService.DeleteAsync(id);
@@ -55,7 +96,16 @@ public class AccountController : ControllerBase
 		return NoContent();
 	}
 
+	/// <summary>
+	/// Update account method
+	/// </summary>
+	/// <param name="id">Account's unique identifier</param>
+	/// <param name="updateAccountRequestDto">Update account request data transfer object</param>
+	/// <returns></returns>
 	[HttpPut("{id:guid}")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Update(Guid id, UpdateAccountRequestDto updateAccountRequestDto)
 	{
 		await _accountService.UpdateAsync(id, updateAccountRequestDto);
