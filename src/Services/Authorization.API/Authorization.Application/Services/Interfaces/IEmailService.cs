6baff7291b317b9e617bb9714f40de471ab1c32d
fles <anotherfles@gmail.com>
2025-01-27 15:43:10 +0300

    Remove old services and interfaces

--- src/Services/Authorization.API/Authorization.Application/Services/Interfaces/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Authorization.Application.Services.Interfaces;
-
-public interface IEmailService
-{
-	public Task SendEmailAsync();
-	public Task VerifyEmailAsync(string verificationToken);
-}
--- src/Services/Authorization.API/Authorization.Application/Services/Interfaces/IVerificationTokenService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Authorization.Application.Services.Interfaces;
-
-public interface IVerificationTokenService
-{
-	public string GenerateToken();
-	public bool VerifyToken(string token);
-}
