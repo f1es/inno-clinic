1be00724f1df476c4127cdecff14554bd2ddd728
fles <anotherfles@gmail.com>
2025-01-29 17:22:10 +0300

    Add endpoints description

--- src/Services/Appointment.API/Appointment.API/Controllers/AppointmentController.cs
+++ src/Services/Appointment.API/Appointment.API/Controllers/AppointmentController.cs
@@ -15,7 +15,14 @@ public class AppointmentController : ControllerBase
 		_appointmentService = appointmentService;
 	}
 
+	/// <summary>
+	/// Get all appointments method
+	/// </summary>
+	/// <returns></returns>
 	[HttpGet]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> GetAll()
 	{
 		var appointments = await _appointmentService.GetAllAsync();
@@ -23,7 +30,16 @@ public class AppointmentController : ControllerBase
 		return Ok(appointments);
 	}
 
+	/// <summary>
+	/// Get appointment by id method
+	/// </summary>
+	/// <param name="id">Appointment's unique identier</param>
+	/// <returns></returns>
 	[HttpGet("{id:guid}", Name = "GetAppointment")]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Get(Guid id)
 	{
 		var appointment = await _appointmentService.GetByIdAsync(id);
@@ -31,7 +47,16 @@ public class AppointmentController : ControllerBase
 		return Ok(appointment);
 	}
 
+	/// <summary>
+	/// Create appointment method
+	/// </summary>
+	/// <param name="appointmentRequestDto">Request appointment data transfer object</param>
+	/// <returns></returns>
 	[HttpPost]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Create(AppointmentRequestDto appointmentRequestDto)
 	{
 		var appoinment = await _appointmentService.CreateAsync(appointmentRequestDto);
@@ -39,7 +64,18 @@ public class AppointmentController : ControllerBase
 		return CreatedAtRoute("GetAppointment", new { id = appoinment.Id }, appoinment);
 	}
 
+	/// <summary>
+	/// Update appointment method
+	/// </summary>
+	/// <param name="id">Appointment's unique identier</param>
+	/// <param name="appointmentRequestDto">Request appointment data transfer object</param>
+	/// <returns></returns>
 	[HttpPut("{id:guid}")]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Update(Guid id, AppointmentRequestDto appointmentRequestDto)
 	{
 		await _appointmentService.UpdateAsync(id, appointmentRequestDto);
@@ -47,12 +83,20 @@ public class AppointmentController : ControllerBase
 		return NoContent();
 	}
 
+	/// <summary>
+	/// Delete appointment method
+	/// </summary>
+	/// <param name="id">Appointment's unique identier</param>
+	/// <returns></returns>
 	[HttpDelete("{id:guid}")]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Delete(Guid id)
 	{
 		await _appointmentService.DeleteAsync(id);
 
 		return NoContent();
 	}
-
 }
--- src/Services/Appointment.API/Appointment.API/Controllers/ResultController.cs
+++ src/Services/Appointment.API/Appointment.API/Controllers/ResultController.cs
@@ -15,7 +15,14 @@ public class ResultController : ControllerBase
 		_resultService = resultService;
 	}
 
+	/// <summary>
+	/// Get all results method
+	/// </summary>
+	/// <returns></returns>
 	[HttpGet]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> GetAll()
 	{
 		var results = await _resultService.GetAllAsync();
@@ -23,7 +30,16 @@ public class ResultController : ControllerBase
 		return Ok(results);
 	}
 
+	/// <summary>
+	/// Get result by id method
+	/// </summary>
+	/// <param name="id">Result's unique ientifier</param>
+	/// <returns></returns>
 	[HttpGet("{id:guid}", Name = "GetResult")]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Get(Guid id)
 	{
 		var result = await _resultService.GetByIdAsync(id);
@@ -31,7 +47,16 @@ public class ResultController : ControllerBase
 		return Ok(result);
 	}
 
+	/// <summary>
+	/// Create result method
+	/// </summary>
+	/// <param name="resultRequestDto">Result's requets data transfer object</param>
+	/// <returns></returns>
 	[HttpPost]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Create(ResultRequestDto resultRequestDto)
 	{
 		var result = await _resultService.CreateAsync(resultRequestDto);
@@ -39,7 +64,16 @@ public class ResultController : ControllerBase
 		return CreatedAtRoute("GetResult", new { id = result.Id }, result);
 	}
 
+	/// <summary>
+	/// Delete result method
+	/// </summary>
+	/// <param name="id">Result's unique ientifier</param>
+	/// <returns></returns>
 	[HttpDelete("{id:guid}")]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status204NoContent)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Delete(Guid id)
 	{
 		await _resultService.DeleteAsync(id);
@@ -47,7 +81,18 @@ public class ResultController : ControllerBase
 		return NoContent();
 	}
 
+	/// <summary>
+	/// Update result method
+	/// </summary>
+	/// <param name="id">Result's unique ientifier</param>
+	/// <param name="resultRequestDto">Result's requets data transfer object</param>
+	/// <returns></returns>
 	[HttpPut("{id:guid}")]
+	[Produces("application/json")]
+	[ProducesResponseType(StatusCodes.Status200OK)]
+	[ProducesResponseType(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	[ProducesResponseType(StatusCodes.Status500InternalServerError)]
 	public async Task<IActionResult> Update(Guid id, ResultRequestDto resultRequestDto)
 	{
 		await _resultService.UpdateAsync(id, resultRequestDto);
