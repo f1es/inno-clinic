ca7a9368f66a422b4097695437b449c5f3fffd33
fles <anotherfles@gmail.com>
2025-01-31 17:47:33 +0300

    Made data base compose file, initialize migration

---
+++ src/Services/Profiles.API/Profiles.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Profiles.Infrastructure.Context;
+
+namespace Profiles.API.Extensions;
+
+public static class DependencyInjection
+{
+	public static void ConfigureDbContext(this IServiceCollection services, WebApplicationBuilder builder)
+	{
+		services.AddDbContext<ProfilesDbContext>(options =>
+		{
+			options.UseSqlServer(builder.Configuration.GetConnectionString("MsSql"));
+		});
+	}
+}
