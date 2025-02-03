d825b1e04ba15600278ea68f765ab078f28a3fbf
fles <anotherfles@gmail.com>
2025-02-03 17:54:59 +0300

    Add services interfaces

---
+++ src/Services/Documents.API/Documents.Application/Services/Interfaces/IDocumentService.cs
@@ -0,0 +1,13 @@
+﻿using Documents.Core.Dtos.Request;
+using Documents.Core.Dtos.Response;
+
+namespace Documents.Application.Services.Interfaces;
+
+public interface IDocumentService
+{
+	public Task<FileResponseDto> CreateAsync(DocumentRequestDto documentRequestDto);
+	public Task DeleteAsync(Guid id);
+	public Task UpdateAsync(Guid id, DocumentRequestDto documentRequestDto);
+	public Task<FileResponseDto> GetByIdAsync(Guid id);
+	public Task<IEnumerable<FileResponseDto>> GetAllAsync();
+}
---
+++ src/Services/Documents.API/Documents.Application/Services/Interfaces/IPhotoService.cs
@@ -0,0 +1,13 @@
+﻿using Documents.Core.Dtos.Request;
+using Documents.Core.Dtos.Response;
+
+namespace Documents.Application.Services.Interfaces;
+
+public interface IPhotoService
+{
+	public Task<FileResponseDto> CreateAsync(PhotoRequestDto photoRequestDto);
+	public Task DeleteAsync(Guid id);
+	public Task UpdateAsync(Guid id, PhotoRequestDto photoRequestDto);
+	public Task<FileResponseDto> GetByIdAsync(Guid id);
+	public Task<IEnumerable<FileResponseDto>> GetAllAsync();
+}
