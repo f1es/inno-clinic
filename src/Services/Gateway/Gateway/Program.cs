8cc70e133864003e8ce852172a4773cb5ec556e7
fles <anotherfles@gmail.com>
2025-02-06 14:20:24 +0300

    Initizalize project, add some libraries

---
+++ src/Services/Gateway/Gateway/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Configuration.AddJsonFile("ocelot.json", false, true);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddOcelot();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+//builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.ConfigureAuthentication(builder);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseSwaggerForOcelotUI(options =>
+	options.PathToSwaggerGenerator = "/swagger/docs");
+
+app.UseHttpsRedirection();
+
+await app.UseOcelot();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
