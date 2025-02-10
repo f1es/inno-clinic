bbce0f8c19f83089d36e6c4f8593bd156e24abe5
fles <anotherfles@gmail.com>
2025-02-10 11:55:51 +0300

    Add environment support for ocelot

--- src/Services/Gateway/Gateway/Program.cs
+++ src/Services/Gateway/Gateway/Program.cs
@@ -6,11 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Configuration.AddJsonFile("ocelot.json", false, true);
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration.AddJsonFile($"ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration.AddJsonFile($"ocelot.{environment}.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddOcelot();
+builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 builder.Services.AddSwaggerGen();
 builder.Services.ConfigureAuthentication(builder);
