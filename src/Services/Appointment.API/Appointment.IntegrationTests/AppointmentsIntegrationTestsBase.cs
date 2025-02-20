394b4c0119692023ec165415c3599e8748843c2e
fles <anotherfles@gmail.com>
2025-02-20 18:04:06 +0300

    Add integration tests for appointment controller

---
+++ src/Services/Appointment.API/Appointment.IntegrationTests/AppointmentsIntegrationTestsBase.cs
@@ -0,0 +1,54 @@
+using Appointment.API.Controllers;
+using Appointment.Application.Mappers.Implementations;
+using Appointment.Application.Mappers.Interfaces;
+using Appointment.Application.Services.Implementations;
+using Appointment.Application.Services.Interfaces;
+using Appointment.Core.Repositories;
+using Appointment.Infrastructure.Context;
+using Appointment.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Testcontainers.PostgreSql;
+
+namespace Appointment.IntegrationTests;
+
+public class AppointmentsIntegrationTestsBase : IAsyncLifetime
+{
+	protected readonly PostgreSqlContainer _postgresContainer;
+	protected DbContextOptions<AppointmentDbContext> _dbContextOptions;
+	protected readonly AppointmentDbContext _context;
+
+	protected readonly IAppointmentsMapper _appointmentsMapper;
+	protected readonly IUnitOfWork _unitOfWork;
+	protected readonly IAppointmentService _appointmentService;
+
+	protected readonly AppointmentController _appointmentController;
+
+    public AppointmentsIntegrationTestsBase()
+    {
+		_postgresContainer = new PostgreSqlBuilder().Build();
+
+		_postgresContainer.StartAsync().Wait();
+
+		_dbContextOptions = new DbContextOptionsBuilder<AppointmentDbContext>()
+			.UseNpgsql(_postgresContainer.GetConnectionString())
+			.Options;
+
+		_context = new AppointmentDbContext(_dbContextOptions);
+		_context.Database.Migrate();
+
+		_unitOfWork = new UnitOfWork(_context);
+		_appointmentsMapper = new AppointmentsMapper();
+		_appointmentService = new AppointmentService(_unitOfWork, _appointmentsMapper);
+		_appointmentController = new AppointmentController(_appointmentService);
+	}
+
+	public Task InitializeAsync()
+	{
+		return _postgresContainer.StartAsync();
+	}
+
+	public Task DisposeAsync()
+	{
+		return _postgresContainer.StopAsync();
+	}
+}
