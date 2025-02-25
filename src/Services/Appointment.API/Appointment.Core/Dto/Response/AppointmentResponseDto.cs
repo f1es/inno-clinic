db867607422de3028e6d23a14e289551e8a3bd14
fles <anotherfles@gmail.com>
2025-02-25 14:40:06 +0300

    Fix dtos, make foreign keys nullable

--- src/Services/Appointment.API/Appointment.Core/Dto/Response/AppointmentResponseDto.cs
+++ src/Services/Appointment.API/Appointment.Core/Dto/Response/AppointmentResponseDto.cs
@@ -2,9 +2,9 @@ namespace Appointment.Core.Dto.Response;
 
 public record AppointmentResponseDto(
 	Guid Id,
-	Guid PatientId,
-	Guid DoctorId,
-	Guid ServiceId,
+	Guid? PatientId,
+	Guid? DoctorId,
+	Guid? ServiceId,
 	DateOnly Date,
 	TimeOnly Time,
 	bool IsApproved);
