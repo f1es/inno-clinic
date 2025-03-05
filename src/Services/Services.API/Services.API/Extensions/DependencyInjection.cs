7713322d26c914c660f0c43d5c6a5cb21b760802
fles <anotherfles@gmail.com>
2025-03-05 12:37:21 +0300

    Add authorization to endpoints

--- src/Services/Services.API/Services.API/Extensions/DependencyInjection.cs
+++ src/Services/Services.API/Services.API/Extensions/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using Services.Infrastructure.Options;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Services.API.Options;
+using Services.Infrastructure.Options;
+using System.Text;
 
 namespace Services.API.Extensions;
 
@@ -9,11 +13,51 @@ public static class DependencyInjection
 		services.Configure<ConnectionStrings>(configuration.GetSection("ConnectionStrings"));
 	}
 
+	private static void ConfigureAuthentication(this IServiceCollection services, IConfiguration configuration)
+	{
+		var jwtOptions = configuration.GetSection("JwtOptions").Get<JwtOptions>();
+
+		services.AddAuthentication(options =>
+		{
+			options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+			options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+		})
+			.AddJwtBearer(options =>
+			{
+				options.Events = new JwtBearerEvents
+				{
+					OnMessageReceived = context =>
+					{
+						if (context.Request.Cookies.ContainsKey("sec"))
+						{
+							context.Token = context.Request.Cookies["sec"];
+						}
+
+						return Task.CompletedTask;
+					}
+				};
+				options.RequireHttpsMetadata = false;
+				options.SaveToken = true;
+				options.ClaimsIssuer = jwtOptions.Issuer;
+				options.TokenValidationParameters = new TokenValidationParameters
+				{
+					ValidateAudience = false,
+					ValidateIssuerSigningKey = true,
+					ValidateLifetime = true,
+					ValidateIssuer = false,
+					IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Key))
+				};
+			});
+
+		services.AddAuthorization();
+	}
+
 	public static void ConfigureApi(this IServiceCollection services, IConfiguration configuration)
 	{
 		services.AddControllers();
 		services.AddEndpointsApiExplorer();
 		services.AddSwaggerGen();
 		services.ConfigureOptions(configuration);
+		services.ConfigureAuthentication(configuration);
 	}
 }
