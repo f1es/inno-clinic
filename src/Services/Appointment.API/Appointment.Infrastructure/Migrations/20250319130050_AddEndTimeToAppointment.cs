97136f824c05bfa0b8c31de5a1f18cd23f61278a
fles <anotherfles@gmail.com>
2025-03-20 13:34:54 +0300

    Separate appointment time to begin time and end time, made migration change dtos

---
+++ src/Services/Appointment.API/Appointment.Infrastructure/Migrations/20250319130050_AddEndTimeToAppointment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Appointment.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEndTimeToAppointment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Time",
+                table: "Appointments",
+                newName: "EndTime");
+
+            migrationBuilder.AddColumn<TimeOnly>(
+                name: "BeginTime",
+                table: "Appointments",
+                type: "time without time zone",
+                nullable: false,
+                defaultValue: new TimeOnly(0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BeginTime",
+                table: "Appointments");
+
+            migrationBuilder.RenameColumn(
+                name: "EndTime",
+                table: "Appointments",
+                newName: "Time");
+        }
+    }
+}
