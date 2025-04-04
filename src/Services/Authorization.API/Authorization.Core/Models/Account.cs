d74526d0e073813e0d8be35d08b202b84374fce5
fles <anotherfles@gmail.com>
2025-04-04 14:55:19 +0300

    Add full name fields to auth database

--- src/Services/Authorization.API/Authorization.Core/Models/Account.cs
+++ src/Services/Authorization.API/Authorization.Core/Models/Account.cs
@@ -12,6 +12,10 @@ public class Account
 	public string? UpdatedBy { get; set; }
 	public DateTime? UpdatedAt { get; set; }
 
+	public string? FirstName { get; set; }
+	public string? LastName { get; set; }
+	public string? MiddleName { get; set; }
+
 	public string? Role { get; set; }
 	public string? RefreshToken { get; set; }
 	public DateTime? RefreshTokenExpirationDate { get; set; }
--- src/Services/Authorization.API/Authorization.Infrastructure/Migrations/20250404115225_AddFullNameRedundancy.Designer.cs
+++ src/Services/Authorization.API/Authorization.Infrastructure/Migrations/20250404115225_AddFullNameRedundancy.Designer.cs
@@ -4,6 +4,7 @@ using Authorization.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Authorization.Infrastructure.Migrations
 {
     [DbContext(typeof(AuthorizationDbContext))]
-    partial class AuthorizationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250404115225_AddFullNameRedundancy")]
+    partial class AddFullNameRedundancy
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
